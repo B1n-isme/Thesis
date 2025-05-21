0627fbe0ef5f9239d720f50377cd5cc4f604f305
Nguyen Truc Linh <10421088@student.vgu.edu.vn>
2025-05-21 17:12:04 +0700

    update neuralforecast

--- nf_ex.py
+++ nf_ex.py
@@ -7,9 +7,12 @@ from utilsforecast.plotting import plot_series
 from neuralforecast import NeuralForecast
 from neuralforecast.models import NBEATS, NHITS, BiTCN
 from neuralforecast.auto import AutoNHITS
-from neuralforecast.losses.pytorch import MAE, MSE, RMSE
+from neuralforecast.losses.pytorch import MAE, MSE, RMSE, MQLoss, DistributionLoss
 from utilsforecast.evaluation import evaluate
+
 import optuna
+from ray import tune
+from ray.tune.search.hyperopt import HyperOptSearch
 
 optuna.logging.set_verbosity(optuna.logging.WARNING)
 logging.getLogger('pytorch_lightning').setLevel(logging.ERROR)
@@ -26,68 +29,90 @@ all_cols = df.columns.tolist()
 exog_vars = [col for col in all_cols if col not in ['ds', 'unique_id', 'y']]
 
 horizon = 1
+levels = [80, 90]
+# nhits_default_config = AutoNHITS.get_default_config(h = horizon, backend="optuna")  
+# def convert_to_tuple(selected_value):
+#     if isinstance(selected_value, str):
+#         return tuple(map(int, selected_value.split(',')))
+#     elif isinstance(selected_value, (list, tuple)):
+#         return tuple(selected_value)
+#     else:
+#         raise ValueError("Unsupported type for kernel size conversion")
 
-nhits_default_config = AutoNHITS.get_default_config(h = horizon, backend="optuna")      
+# def config_nhits(trial):
+#     n_pool_kernel_size = trial.suggest_categorical(  # MaxPooling Kernel size
+#         "n_pool_kernel_size",
+#         ["1,1,1,1,1", "2,2,2,2,2", "4,4,4,4,4", "8,4,2,1,1"]
+#     )
+#     n_freq_downsample = trial.suggest_categorical(   # Interpolation expressivity ratios
+#         "n_freq_downsample",
+#         ["8,4,2,1,1", "1,1,1,1,1"]
+#     )
+#     return {
+#         "input_size": trial.suggest_categorical(          # Length of input window
+#             "input_size", (48, 48*2, 48*3)                
+#         ),                                                
+#         "start_padding_enabled": True,                                          
+#         "n_blocks": 5 * [1],                              # Length of input window
+#         "mlp_units": 5 * [[64, 64]],                      # Length of input window
+#         "n_pool_kernel_size": convert_to_tuple(n_pool_kernel_size),
+#         "n_freq_downsample": convert_to_tuple(n_freq_downsample),
+#         "learning_rate": trial.suggest_float(             # Initial Learning rate
+#             "learning_rate",
+#             low=1e-4,
+#             high=1e-2,
+#             log=True,
+#         ),            
+#         "scaler_type": None,                              # Scaler type
+#         "max_steps": 1000,                                # Max number of training iterations
+#         "batch_size": trial.suggest_categorical(          # Number of series in batch
+#             "batch_size",
+#             (1, 4, 10),
+#         ),                   
+#         "windows_batch_size": trial.suggest_categorical(  # Number of windows in batch
+#             "windows_batch_size",
+#             (128, 256, 512),
+#         ),      
+#         "random_seed": trial.suggest_int(                 # Random seed   
+#             "random_seed",
+#             low=1,
+#             high=20,
+#         ),                      
+#     }
 
-def convert_to_tuple(selected_value):
-    if isinstance(selected_value, str):
-        return tuple(map(int, selected_value.split(',')))
-    elif isinstance(selected_value, (list, tuple)):
-        return tuple(selected_value)
-    else:
-        raise ValueError("Unsupported type for kernel size conversion")
 
-def config_nhits(trial):
-    n_pool_kernel_size = trial.suggest_categorical(  # MaxPooling Kernel size
-        "n_pool_kernel_size",
-        ["1,1,1,1,1", "2,2,2,2,2", "4,4,4,4,4", "8,4,2,1,1"]
-    )
-    n_freq_downsample = trial.suggest_categorical(   # Interpolation expressivity ratios
-        "n_freq_downsample",
-        ["8,4,2,1,1", "1,1,1,1,1"]
-    )
-    return {
-        "input_size": trial.suggest_categorical(          # Length of input window
-            "input_size", (48, 48*2, 48*3)                
-        ),                                                
-        "start_padding_enabled": True,                                          
-        "n_blocks": 5 * [1],                              # Length of input window
-        "mlp_units": 5 * [[64, 64]],                      # Length of input window
-        "n_pool_kernel_size": convert_to_tuple(n_pool_kernel_size),
-        "n_freq_downsample": convert_to_tuple(n_freq_downsample),
-        "learning_rate": trial.suggest_float(             # Initial Learning rate
-            "learning_rate",
-            low=1e-4,
-            high=1e-2,
-            log=True,
-        ),            
-        "scaler_type": None,                              # Scaler type
-        "max_steps": 1000,                                # Max number of training iterations
-        "batch_size": trial.suggest_categorical(          # Number of series in batch
-            "batch_size",
-            (1, 4, 10),
-        ),                   
-        "windows_batch_size": trial.suggest_categorical(  # Number of windows in batch
-            "windows_batch_size",
-            (128, 256, 512),
-        ),      
-        "random_seed": trial.suggest_int(                 # Random seed   
-            "random_seed",
-            low=1,
-            high=20,
-        ),                      
+nhits_default_config = AutoNHITS.get_default_config(h = horizon, backend="ray")      
+
+config_nhits = {
+       "learning_rate": tune.choice([1e-3]),                                     # Initial Learning rate
+       "max_steps": tune.choice([1000]),                                         # Number of SGD steps
+       "input_size": tune.choice([5 * horizon]),                                 # input_size = multiplier * horizon
+       "batch_size": tune.choice([7]),                                           # Number of series in windows
+       "windows_batch_size": tune.choice([256]),                                 # Number of windows in batch
+       "n_pool_kernel_size": tune.choice([[2, 2, 2], [16, 8, 1]]),               # MaxPool's Kernelsize
+       "n_freq_downsample": tune.choice([[168, 24, 1], [24, 12, 1], [1, 1, 1]]), # Interpolation expressivity ratios
+       "activation": tune.choice(['ReLU']),                                      # Type of non-linear activation
+       "n_blocks":  tune.choice([[1, 1, 1]]),                                    # Blocks per each 3 stacks
+       "mlp_units":  tune.choice([[[512, 512], [512, 512], [512, 512]]]),        # 2 512-Layers per block for each stack
+       "interpolation_mode": tune.choice(['linear']),                            # Type of multi-step interpolation
+       "val_check_steps": tune.choice([100]),                                    # Compute validation every 100 epochs
+       "random_seed": tune.randint(1, 10),
     }
 
+
 models = [NHITS(h = horizon,
+                loss=MAE(),
                 max_steps=100,
                 input_size = 5*horizon,
                 hist_exog_list = exog_vars,
                 scaler_type = 'standard'),
         AutoNHITS(h=1,
-                  loss=MAE(),
+                #   loss=MQLoss(level=levels), # probabilistic forecasting
+                loss=DistributionLoss("Normal", level=[90]), # Uncertainty quantification
                   config=config_nhits,
-                  search_alg=optuna.samplers.TPESampler(),
-                  backend='optuna',
+                #   search_alg=optuna.samplers.TPESampler(),
+                  search_alg=HyperOptSearch(),
+                  backend='ray',
                   num_samples=20)
 ]
 
@@ -95,7 +120,8 @@ nf = NeuralForecast(models=models,
                     freq='D', 
                     local_scaler_type='standard')
 
-cv_df = nf.cross_validation(df, n_windows=4, step_size=horizon//2, verbose=1, refit=True)
+# set val_size & test_size
+Y_hat_df = nf.cross_validation(df, n_windows=4, step_size=horizon//2, verbose=1, refit=True) 
 
 # nf.fit(df=df, val_size=horizon*2)
 
@@ -104,7 +130,7 @@ cv_df = nf.cross_validation(df, n_windows=4, step_size=horizon//2, verbose=1, re
 results = nf.models[0].results.trials_dataframe()
 results.drop(columns='user_attrs_ALL_PARAMS')
 
-evaluation_df = evaluate(cv_df.drop(columns='cutoff'), metrics=[MSE, MAE, RMSE])
+evaluation_df = evaluate(Y_hat_df.drop(columns='cutoff'), metrics=[MSE, MAE, RMSE])
 evaluation_df['best_model'] = evaluation_df.drop(columns=['metric', 'unique_id']).idxmin(axis=1)
 evaluation_df.head()
 
@@ -113,7 +139,7 @@ summary_df = summary_df.reset_index()
 summary_df.columns = ['metric', 'model', 'nr. of unique_ids']
 summary_df
 
-Y_hat_df1 = nf.predict()
+Y_hat_df1 = nf.predict(df= test_df, level = [90]) # generate conformal intervals
 Y_hat_df1 = Y_hat_df1.reset_index()
 Y_hat_df1.head()
 
